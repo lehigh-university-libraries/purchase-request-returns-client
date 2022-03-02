960d13d768483ca4a5aae4a244e545fff733725a
Maccabee Levine <msl321@lehigh.edu>
2022-03-02 15:51:09 -0500

    Rename and move MVC config

--- src/main/java/edu/lehigh/libraries/purchase_request/returns_client/config/WebMvcConfig.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/returns_client/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package edu.lehigh.libraries.purchase_request.returns_client;
+package edu.lehigh.libraries.purchase_request.returns_client.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class Config implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
