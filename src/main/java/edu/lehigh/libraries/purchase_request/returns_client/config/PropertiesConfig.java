f88e48e51f70f8266ea8c87c2463d5be917a295b
Maccabee Levine <msl321@lehigh.edu>
2022-03-03 08:53:38 -0500

    Manage/document config properties and allow disabling security for test purposes

---
+++ src/main/java/edu/lehigh/libraries/purchase_request/returns_client/config/PropertiesConfig.java
@@ -0,0 +1,55 @@
+package edu.lehigh.libraries.purchase_request.returns_client.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix="returns-client")
+@EnableAsync
+@Getter @Setter
+public class PropertiesConfig {
+    
+    private Database db;
+
+    /**
+     * Disables all security.  DO NOT USE IN PRODUCTION.
+     */
+    private boolean disableSecurity = false;
+
+    @Getter @Setter
+    /**
+     * Connection properties for database used for workflow proxy metadata.
+     * 
+     * This database is not used for the actual workflow items backed by a WorkflowService.
+     */
+    public static class Database {
+
+        /**
+         * Database hostname
+         */
+        private String host;
+
+        /**
+         * Database name
+         */
+        private String name;
+
+        /**
+         * Database username
+         */
+        private String username;
+
+        /**
+         * Database password
+         */
+        private String password;
+
+    }
+
+}
+
+
--- src/main/java/edu/lehigh/libraries/purchase_request/returns_client/security/SecurityConfig.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/returns_client/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package edu.lehigh.libraries.purchase_request.returns_client.security;
 
+import edu.lehigh.libraries.purchase_request.returns_client.config.PropertiesConfig;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,56 +16,63 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-      .csrf().disable()
-      .authorizeRequests().anyRequest().authenticated().and()
-      .formLogin().permitAll() 
-        .and() 
-      .logout().invalidateHttpSession(true).clearAuthentication(true).permitAll(); 
-    return httpSecurity.build();
-  }
-  
-  /*
-  @Bean
-  public SecurityFilterChain disableAuthentication(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-      .httpBasic().disable(); 
-    return httpSecurity.build();
-  }
-  */
+	private PropertiesConfig properties;
 
-  @Bean
-  public UserDetailsService userDetailsService() {
-      return new UserDetailsService() { 
-          @Autowired
-          private UserRepository userRepository;
+	SecurityConfig(PropertiesConfig properties) {
+		this.properties = properties;
+	}
 
-          @Override
-          public UserDetails loadUserByUsername(String username) {
-              User user = Optional.ofNullable(userRepository.findByUsername(username))
-                  .orElseThrow(() -> new UsernameNotFoundException("Cannot find user: " + username));
-              return new ReturnsUserDetails(user);
-          }
-      };
-  }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		final boolean disableSecurity = properties.isDisableSecurity();
+		if (disableSecurity) {
+			log.warn("Security is DISABLED");
+			httpSecurity
+					.httpBasic().disable();
+		} else {
+			httpSecurity
+					.csrf().disable()
+					.authorizeRequests().anyRequest().authenticated().and()
+					.formLogin().permitAll()
+					.and()
+					.logout().invalidateHttpSession(true).clearAuthentication(true).permitAll();
+		}
+		return httpSecurity.build();
+	}
 
-  @Bean
-  public static BCryptPasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-  }
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+			@Autowired
+			private UserRepository userRepository;
 
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-      authProvider.setUserDetailsService(userDetailsService());
-      authProvider.setPasswordEncoder(passwordEncoder());
-      return authProvider;
-  }
+			@Override
+			public UserDetails loadUserByUsername(String username) {
+				User user = Optional.ofNullable(userRepository.findByUsername(username))
+						.orElseThrow(() -> new UsernameNotFoundException("Cannot find user: " + username));
+				return new ReturnsUserDetails(user);
+			}
+		};
+	}
+
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
 
 }
