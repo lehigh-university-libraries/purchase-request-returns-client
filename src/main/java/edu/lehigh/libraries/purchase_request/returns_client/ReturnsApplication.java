962764dd061e7f2e79eaa25d8a15d0a2792f8153
Maccabee Levine <msl321@lehigh.edu>
2022-04-12 15:32:18 -0400

    Package app for .war deployment

--- src/main/java/edu/lehigh/libraries/purchase_request/returns_client/ReturnsApplication.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/returns_client/ReturnsApplication.java
@@ -3,13 +3,20 @@ package edu.lehigh.libraries.purchase_request.returns_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @ConditionalOnWebApplication
 @Slf4j
-public class ReturnsApplication {
+public class ReturnsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReturnsApplication.class);
+	}
 
 	public static void main(String[] args) throws Exception {
 		log.info("Starting the Returns Application");
