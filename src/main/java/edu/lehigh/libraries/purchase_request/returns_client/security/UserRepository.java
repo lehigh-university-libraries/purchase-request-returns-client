3d6dfd5d8f3ee5bf6c4b8e4f4db67e1a21602a1f
Maccabee Levine <msl321@lehigh.edu>
2022-12-12 10:48:37 -0500

    Remove redundant interface

--- src/main/java/edu/lehigh/libraries/purchase_request/returns_client/security/UserRepository.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/returns_client/security/UserRepository.java
@@ -1,9 +1,7 @@
 package edu.lehigh.libraries.purchase_request.returns_client.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
